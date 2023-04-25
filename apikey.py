a235d07159519c22e8e5e2ee7d6f356c2ebbb19e
norainthere <129417183+norainthere@users.noreply.github.com>
2023-04-25 09:21:28 -0400

    chatbot

---
+++ apikey.py
@@ -0,0 +1,1 @@
+apikey = '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'
---
+++ app.py
@@ -0,0 +1,12 @@
+# Bring in deps
+import os
+from apikey import apikey
+
+import streamlit as st
+from langchain.llms import OpenAI
+
+os.environ['OPEN_API_KEY']= apikey
+
+# App framework
+st.title('🦜🔗Wend1')
+prompt = st.text_input('Plug in your prompt here')
